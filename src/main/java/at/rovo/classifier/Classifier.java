79d4c84729aee4f7784562ce53e5e5c1ea13fbea
RovoMe <rovo@gmx.at>
2013-07-08 14:09:59 +0200

    Did not change Classifier to match log4j2 recommended log-messages

--- src/main/java/at/rovo/classifier/Classifier.java
+++ src/main/java/at/rovo/classifier/Classifier.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -17,18 +16,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * <p>Core class for any classifier. It requires extending classes to implement some
- * typical methods for any classifier as <code>train()</code> and <code>classify()</code>.
+ * <p>
+ * Core class for any classifier. It requires extending classes to implement
+ * some typical methods for any classifier as <code>train()</code> and
+ * <code>classify()</code>.
  * </p>
- * <p>Further it is able to save and load its data from and to disk, therefore it requires
- * that categories as well as features have to implement {@link Serializable}.</p>
- *
- * @param <F> Type of the features this classifier has to deal with
- * @param <C> Type of the categories this classifier supports
+ * <p>
+ * Further it is able to save and load its data from and to disk, therefore it
+ * requires that categories as well as features have to implement
+ * {@link Serializable}.
+ * </p>
+ * 
+ * @param <F>
+ *            Type of the features this classifier has to deal with
+ * @param <C>
+ *            Type of the categories this classifier supports
  * @author Roman Vottner
  */
 public abstract class Classifier<F extends Serializable, C extends Serializable>
 {
+	/** The logger of this class **/
 	private static Logger logger = LogManager.getLogger(Classifier.class.getName());
 	/** The actual training data which it uses to classify features **/
 	protected TrainingData<F,C> trainingData = null;
@@ -39,7 +46,9 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	private String name = "";
 	
 	/**
-	 * <p>Instantiates necessary data structures for subclasses.</p>
+	 * <p>
+	 * Instantiates necessary data structures for subclasses.
+	 * </p>
 	 */
 	public Classifier()
 	{
@@ -47,10 +56,13 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	}
 	
 	/**
-	 * <p>Instantiates necessary data structures for subclasses and
-	 * assign a name to the classifier.</p>
+	 * <p>
+	 * Instantiates necessary data structures for subclasses and assign a name
+	 * to the classifier.
+	 * </p>
 	 * 
-	 * @param name The name of the classifier
+	 * @param name
+	 *            The name of the classifier
 	 */
 	public Classifier(String name)
 	{
@@ -58,9 +70,12 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	}
 	
 	/**
-	 * <p>Sets the name of the classifiers' instance</p>
+	 * <p>
+	 * Sets the name of the classifiers' instance
+	 * </p>
 	 * 
-	 * @param name New name of the classifier
+	 * @param name
+	 *            New name of the classifier
 	 */
 	public void setName(String name)
 	{
@@ -68,7 +83,9 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	}
 	
 	/**
-	 * <p>Returns the name of the classifiers' instance</p>
+	 * <p>
+	 * Returns the name of the classifiers' instance
+	 * </p>
 	 * 
 	 * @return Name of the classifier
 	 */
@@ -78,12 +95,16 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	}
 	
 	/**
-	 * <p>Persists a {@link Classifier}s data object via java object serialization 
-	 * to a file in a defined directory.</p>
+	 * <p>
+	 * Persists a {@link Classifier}s data object via java object serialization
+	 * to a file in a defined directory.
+	 * </p>
 	 * 
-	 * @param directory The directory the {@link Classifier} should be saved in
-	 * @param name The name of the {@link File} which will hold the bytes of the
-	 *             persisted object.
+	 * @param directory
+	 *            The directory the {@link Classifier} should be saved in
+	 * @param name
+	 *            The name of the {@link File} which will hold the bytes of the
+	 *            persisted object.
 	 */
 	public void saveData(File directory, String name)
 	{
@@ -99,7 +120,7 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 			} 
 			catch (IOException e) 
 			{
-				logger.error(e);
+				logger.catching(e);
 			}
 			finally
 			{
@@ -111,21 +132,20 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 					fos.close();
 			}
 		} 
-		catch (FileNotFoundException e) 
-		{
-			logger.error(e);
-		} 
 		catch (IOException e)
 		{
-			logger.error(e);
+			logger.catching(e);
 		}
 	}
 	
 	/**
-	 * <p>Loads data from a previous classification into the classifier.</p>
+	 * <p>
+	 * Loads data from a previous classification into the classifier.
+	 * </p>
 	 * 
-	 * @param serializedObject A reference to the {@link File} representing
-	 *                         the serialized object
+	 * @param serializedObject
+	 *            A reference to the {@link File} representing the serialized
+	 *            object
 	 * @return true if the data could be loaded; false otherwise
 	 */
 	@SuppressWarnings("unchecked")
@@ -143,22 +163,14 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 				if (obj instanceof TrainingData)
 				{
 					data = (TrainingData<F, C>)obj;
-					if (logger.isInfoEnabled())
-						logger.info("Found trained data for: "+data);
+					logger.info("Found trained data for: {}", data);
 				}
 				else
-					if (logger.isErrorEnabled())
-						logger.error("File is not a valid data object for this classifier!");
+					logger.error("File is not a valid data object for this classifier!");
 			}
-			catch (IOException ioEx)
+			catch (IOException | ClassNotFoundException e) 
 			{
-				if (logger.isErrorEnabled())
-					logger.error(ioEx.getLocalizedMessage(), ioEx);
-			} 
-			catch (ClassNotFoundException e) 
-			{
-				if (logger.isErrorEnabled())
-					logger.error(e.getLocalizedMessage(), e);
+				logger.catching(e);
 			}
 			finally
 			{
@@ -170,15 +182,9 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 					fis.close();
 			}
 		}
-		catch (FileNotFoundException fnfEx)
-		{
-			if (logger.isErrorEnabled())
-				logger.error(fnfEx.getLocalizedMessage(), fnfEx);
-		}
 		catch (IOException e) 
 		{
-			if (logger.isErrorEnabled())
-				logger.error(e.getLocalizedMessage(), e);
+			logger.catching(e);
 		}
 		
 		if (data != null)
@@ -190,73 +196,94 @@ public abstract class Classifier<F extends Serializable, C extends Serializable>
 	}
 	
 	/**
-	 * <p>Trains the classifier for a single feature.</p>
-	 * <p>The category is used to label the item/s, f.e. to be a good 
-	 * ('good') example or to be within ('in') an articles content. This
-	 * is later on used to decide if a certain feature is more likely to
-	 * be in a certain category or not.</p>
+	 * <p>
+	 * Trains the classifier for a single feature.
+	 * </p>
+	 * <p>
+	 * The category is used to label the item/s, f.e. to be a good ('good')
+	 * example or to be within ('in') an articles content. This is later on used
+	 * to decide if a certain feature is more likely to be in a certain category
+	 * or not.
+	 * </p>
 	 * 
-	 * @param item A {@link String} contains only one word that 
-	 *             should be trained for a given category.
-	 * @param category The label of the item, f.e. 'good' or 'in', 
-	 *                 the classifier should use to decide later on
-	 *                 what results to predict
+	 * @param item
+	 *            A {@link String} contains only one word that should be trained
+	 *            for a given category.
+	 * @param category
+	 *            The label of the item, f.e. 'good' or 'in', the classifier
+	 *            should use to decide later on what results to predict
 	 */
 	public abstract void train(F item, C category);
 	
 	/**
-	 * <p>Trains the classifier with the specified items to be in a
-	 * certain category.</p>
-	 * <p>The category is used to label the item/s, f.e. to be a good 
-	 * ('good') example or to be within ('in') an articles content. This
-	 * is later on used to decide if a certain feature is more likely to
-	 * be in a certain category or not.</p>
+	 * <p>
+	 * Trains the classifier with the specified items to be in a certain
+	 * category.
+	 * </p>
+	 * <p>
+	 * The category is used to label the item/s, f.e. to be a good ('good')
+	 * example or to be within ('in') an articles content. This is later on used
+	 * to decide if a certain feature is more likely to be in a certain category
+	 * or not.
+	 * </p>
 	 * 
-	 * @param items An array of {@link String} containing all the words 
-	 *              that should be trained for a given category.
-	 * @param category The label of the item, f.e. 'good' or 'in', 
-	 *                 the classifier should use to decide later on
-	 *                 what results to predict
+	 * @param items
+	 *            An array of {@link String} containing all the words that
+	 *            should be trained for a given category.
+	 * @param category
+	 *            The label of the item, f.e. 'good' or 'in', the classifier
+	 *            should use to decide later on what results to predict
 	 */
 	public abstract void train(F[] items, C category);
 	
 	/**
-	 * <p>Trains the classifier with the specified items to be in a
-	 * certain category.</p>
-	 * <p>The category is used to label the item/s, f.e. to be a good 
-	 * ('good') example or to be within ('in') an articles content. This
-	 * is later on used to decide if a certain feature is more likely to
-	 * be in a certain category or not.</p>
+	 * <p>
+	 * Trains the classifier with the specified items to be in a certain
+	 * category.
+	 * </p>
+	 * <p>
+	 * The category is used to label the item/s, f.e. to be a good ('good')
+	 * example or to be within ('in') an articles content. This is later on used
+	 * to decide if a certain feature is more likely to be in a certain category
+	 * or not.
+	 * </p>
 	 * 
-	 * @param items A {@link List} of {@link String}s containing all 
-	 *              the words that should be trained for a given category.
-	 * @param category The label of the item, f.e. 'good' or 'in', 
-	 *                 the classifier should use to decide later on
-	 *                 what results to predict
+	 * @param items
+	 *            A {@link List} of {@link String}s containing all the words
+	 *            that should be trained for a given category.
+	 * @param category
+	 *            The label of the item, f.e. 'good' or 'in', the classifier
+	 *            should use to decide later on what results to predict
 	 */
 	public abstract void train(List<F> items, C category);
 	
 	/**
-	 * <p>Tries to predict the category a certain item is most likely 
-	 * to be in</p>
+	 * <p>
+	 * Tries to predict the category a certain item is most likely to be in
+	 * </p>
 	 * 
-	 * @param item The item which category should be predicted
+	 * @param item
+	 *            The item which category should be predicted
 	 * @return The predicted category the item is most likely to be in
 	 */
 	public abstract C classify(F item);
 	
 	/**
-	 * <p>Tries to predict the category certain items are most likely 
-	 * to be in</p>
+	 * <p>
+	 * Tries to predict the category certain items are most likely to be in
+	 * </p>
 	 * 
-	 * @param items The items which category should be predicted
+	 * @param items
+	 *            The items which category should be predicted
 	 * @return The predicted category the items are most likely to be in
 	 */
 	public abstract C classify(F[] items);
 	
 	/**
-	 * <p>Divides the dataset into a training and a test set. The latter is used
-	 * to estimate the classification accuracy of the trained model.</p>
+	 * <p>
+	 * Divides the dataset into a training and a test set. The latter is used to
+	 * estimate the classification accuracy of the trained model.
+	 * </p>
 	 */
 	public void crossValidation()
 	{
